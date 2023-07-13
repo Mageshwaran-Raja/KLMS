c293e8514e7ae4c21ae390bff93d05625573ade1
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-07-13 12:28:28 +0530

    UserLoginActivity to Log User Login Details

---
+++ KLMS.Domain/UserLoginActivity.cs
@@ -0,0 +1,12 @@
+﻿using KLMS.Domain.Common;
+
+namespace KLMS.Domain
+{
+    public class UserLoginActivity : BaseEntity
+    {
+        public Users User { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime LoginTime { get; set; }
+        public DateTime LogoutTime { get; set; }
+    }
+}
